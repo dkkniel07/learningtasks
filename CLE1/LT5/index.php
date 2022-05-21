149cda30411dbf379ccab597e1d5cfc1e016e515
dkkniel07 <100913139+dkkniel07@users.noreply.github.com>
2022-05-21 23:06:40 +0800

    Add files via upload

---
+++ CLE1/LT5/index.php
@@ -0,0 +1,128 @@
+<!----Rogel---- Our Fam Business- ICA ink-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>ICA Ink </title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+<!------ Google Fonts ------>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"/>
+<link rel="stylesheet" href="style.css" />
+</head>
+  <body>
+    <!------ Scroll To Top Button ------>
+    <button id="scroll-top">
+      <i class="fas fa-arrow-up"></i>
+    </button>
+    <!------ Header ------>
+    <header>
+      <nav>
+        <a href="#home" id="logo">ICA Ink</a>
+        <ul id="nav-bar">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#design">Design</a></li>
+          <li><a href="#banner">Our Facebook Page</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!------ content ------>
+    <section class="hero" id="home">
+      <img src="header-shape.svg" id="header-shape" />
+      <div class="hero-content">
+        <h1>ICA Ink</h1>
+        <p>
+            PERSONALIZED GIFT ITEMS | TSHIRT PRINTING | TARPAULIN | PANAFLEX | DECALS | SIGNAGES | BANNERS
+        </p>
+        <div class="btn-container">
+            <a href="https://www.facebook.com/ICAPrintADS/?ref=page_internal"><button class="primary-btn btn">ORDER NOW</button></a>
+            <a href="https://www.facebook.com/ICAPrintADS/?ref=page_internal"> <button class="secondary-btn btn">OUR FACEBOOK PAGE</button></a>
+        </div>
+      </div>
+      <div class="hero-img">
+        <img src="images/icaink.jpg" />
+      </div>
+    </section>
+        <!------ BANNER ------>
+        <section class="banner" id="banner">
+            <h2>BANNER</h2>
+            <div class="row">
+              <div class="column">
+                <img src="images/banner.jpg" />
+                <h3>"LET'S PRINT THE BRILLIANT LIFE"</h3>
+                <div class="banner-btn">
+                    <a href="https://www.facebook.com/ICAPrintADS/?ref=page_internal"><button class="btn secondary-btn">Learn More</button></a>
+                </div>
+              </div>
+            </div>
+          </section>
+    <!------MISSION VISION------>
+    <section class="features" id="features">
+      <h2>MISSION AND VISION</h2>
+      <p class="section-desc"></p>
+      <div class="row">
+        <div class="column">
+          <i class="fas fa-star"></i>
+          <h3>MISSION</h3>
+          <p>
+            We strive to provide our Clients with the best individualised printing solutions and exceptional services, in the fastest, most economical and artistic way possible without sacrificing the quality and using the latest in technology. Our ultimate goal is to create value, forming lasting relationships and enable our Clients to Grow!
+          </p>
+        </div>
+        <div class="column">
+          <i class="fas fa-thumbs-up"></i>
+          <h3>VISION</h3>
+          <p>
+            To become the most recognized and trusted Print and Marketing Solutions Provider. We will continue to evolve, generate plenty of business opportunities and will constantly strive for successful results in every area of our business.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!------ DESIGN------>
+    <section class="design" id="design">
+      <h2>DESIGNS</h2>
+      <p class="section-desc">
+        We provide a learning experience that is not available elsewhere. We
+        have over 50,000 happy students.
+      </p>
+      <div class="row">
+        <div class="column">
+          <div class="design-image">
+            <img src="images/1.png" />
+          </div>
+          <h3>BLACK</h3>
+        </div>
+        <div class="column">
+          <div class="design-image">
+            <img src="images/2.png" />
+          </div>
+          <h3>BLUE</h3>
+        </div>
+        <div class="column">
+            <div class="design-image">
+              <img src="images/3.png" />
+            </div>
+            <h3>WHITE</h3>
+          </div>
+      </div>
+    </section>
+    <!------ Footer ------>
+    <footer>
+      <div class="row footer-contact">
+        <div class="column">
+          <h4>Phone No</h4>
+          <p>0919 513 2307</p>
+        </div>
+        <div class="column">
+          <h4>Email</h4>
+          <p>icaprintads@gmail.com</p>
+        </div>
+      </div>
+      <p class="footer-copyright">
+        Opens 9:00 AM to 5:00 PM
+      </p>
+    </footer>
+    <!------ Script ------>
+    <script src="script.js"></script>
+  </body>
+</html>
